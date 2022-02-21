be6d091195acddf5e182eeb20b22c2f4045d56b5
JarneVK <jarne.vankerckhove24@gmail.com>
2022-02-21 19:59:06 +0100

    versie1.0

--- BachelorProef/HeaderMatrix.h
+++ BachelorProef/HeaderMatrix.h
@@ -1,1 +1,5 @@
+
+
+// uart.c
 void uartsetup();
+//int sendData_usart0(uint8_t hexgetal);
--- BachelorProef/uart.c
+++ BachelorProef/uart.c
@@ -23,7 +23,7 @@
 #define initIrcomUsart(ARG) \
 NAME3(USART,ARG,_BAUDL) = 0xB6;\
 NAME3(USART,ARG,_BAUDH) = 0x02;\
-NAME3(USART,ARG,_CTRLC) = 0xA3;\
+NAME3(USART,ARG,_CTRLC) = 0x3E;  //00111110 -> 9bit verzending
 
 
 void uartsetup(){
@@ -44,8 +44,9 @@ void uartsetup(){
     je kan ENKEL in het register schrijven als DREIF (in usartn.status) bit op 1 staat
 */
 
-int sendData(uint8_t hexgetal){   // returnt een 0 als het kan verzonden zorden anders een 1
+int sendData_usart0(int hexgetal){   // returnt een 0 als het kan verzonden zorden anders een 1
     if(USART0_STATUS&(1<<5)){  // get de DREIF bit
+		USART0_TXDATAH &= ~(1<<0);	// zet bit op 0
         USART0_TXDATAL = hexgetal;
         return 0;
     }    
@@ -53,6 +54,28 @@ int sendData(uint8_t hexgetal){   // returnt een 0 als het kan verzonden zorden 
         // register is nog niet geschift > even wachten
         return 1;
     }
+}
+
+int sendACK_usart0(){
+	if(USART0_STATUS&(1<<5)){  // get de DREIF bit
+		USART0_TXDATAH &= ~(1<<0);	// zet bit op 0
+		USART0_TXDATAL = 0x00;
+		return 0;
+	}
+	else {
+		return 1;
+	}
+}
+
+int sendNACK_usart0(){
+	if(USART0_STATUS&(1<<5)){  // get de DREIF bit
+		USART0_TXDATAH |= (1<<0);	// zet bit op 1
+		USART0_TXDATAL = 0xFF;
+		return 0;
+	}
+	else {
+		return 1;
+	}
 }
 
 /* get data : in RXDATAH, bit 7 zegt of er data in de buffer zit -> eerste hiernaar kijken 
@@ -66,6 +89,29 @@ int sendData(uint8_t hexgetal){   // returnt een 0 als het kan verzonden zorden 
     - checken of de data goed is toegekomen met PERR/FERR
     - als alles correct is -> RXDATAL inlezen en register laten schiften
 */
+
+int readRegister_usart0(){      // geeft 8 bits terug + 1 bit als packetnummer 
+	int bits[2];
+	if(USART0_RXDATAH&(1<<7)){	// kijken naar 7de bit
+		bits[0] = USART0_RXDATAH;
+		bits[1] = USART0_RXDATAL; 
+		
+		if(bits[0]&(1<<2) || bits[0]&(1<<1)){	// kijken of er geen frame of parity errors zijn
+			sendNACK_usart0();			// 0xAA voorlopig als NACK
+			return 1;
+		}
+		
+		//else if checken op NACK moet nog worden toegevoegd -> vraag hoe gaan we dit naar buiten brengen via de int?
+		
+		else{
+			sendACK_usart0();
+			return bits[1];
+		}	
+	}
+	else{
+		return 1;	// nog geen data beschikbaar
+	}
+}
 
 
 
