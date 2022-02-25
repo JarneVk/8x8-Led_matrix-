b03be6aec949b9a231772765d6d0d23f272d8220
JarneVk <jarne.vankerckhove24@gmail.com>
2022-02-25 18:52:39 +0100

    low level led matrix hard code op PB5

--- low_level_aansturing_leds.c
+++ low_level_aansturing_leds.c
@@ -4,6 +4,8 @@ aansturing Leds : APA102
 	-De LED frames, hoe meer led frames achtereen hoe verder het zal doorschuiven in 1 verzending 
 	-Een LED frame -> 3bit van 1, 5 bits global, 8bit Blue, 8bit Green, 8bit Red	=> de global bit zorgt voor de brightness
 	-End frame : 32 bit 1, 4x bits van 1
+
+    op de pin PB5 staat de output die gekoppeld moet worden aan SDI van de apa102 
 */
 
 #include <avr/io.h>
@@ -11,13 +13,30 @@ aansturing Leds : APA102
 
 void initLedPoorten(){
     //initialisatie van de gebruikte poorten (als output zetten)
+    PORTB_DIR |= PIN5_bm;   // pin5_bm is een standaard bitmap functie van avr/io
+
 }
 
 
-int writeToLed(&bitArray[29]){
+int writeToLed(char bitArray[],int aantalAangestuurdeLeds){
 
+    for(int i=0; i<32; ++i){    //32x0
+        PORTB_OUT &= ~PIN5_bm;
+    }
+    for(int i=0; i<aantalAangestuurdeLeds;++i){             // led info doorsturen
+        for(int j=0; j<29;++j){
+            if(bitArray[(aantalAangestuurdeLeds*29)+j]){
+                PORTB_OUT |= PIN5_bm;
+            }
+            else{
+                PORTB_OUT &= ~PIN5_bm;
+            }
+        }
+    }
 
-
+    for(int i=0; i<32; ++i){    //32x1
+        PORTB_OUT |= PIN5_bm;
+    }
 
 }
 
