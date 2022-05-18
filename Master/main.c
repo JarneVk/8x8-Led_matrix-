f0d8aff7fbc6fd78ee62ae7c4c18bf51bb6d0690
lucascool12 <71119084+lucascool12@users.noreply.github.com>
2022-05-18 17:20:03 +0200

    eeprom lezen

--- Master/main.c
+++ Master/main.c
@@ -8,6 +8,12 @@ int main(void)
     USART3_Init();
     initLedPoorten();
     initGlobalVariables();
+    Led inputLogo[8][8];
+    if(readFromEeprom(inputString, input_red, input_green, input_blue, inputLogo, &input_brightness)){
+        getUserInput(inputString, input_red, input_blue, input_green, input_brightness);
+        getUserLogo(inputLogo);
+    }
+
     uarts_setup();
     
     //string in matrix zetten
